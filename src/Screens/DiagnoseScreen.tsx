ec095fbd0146dea7c36182693e2c7c583bb4ca10
R8P <abdulkadirozkan@yandex.com>
2023-03-16 02:16:11 +0300

    edit screen text

--- src/Screens/DiagnoseScreen.tsx
+++ src/Screens/DiagnoseScreen.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const DiagnoseScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>DiagnoseScreen</Text>
     </View>
   )
@@ -11,4 +11,10 @@ const DiagnoseScreen = () => {
 
 export default DiagnoseScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
--- src/Screens/MyGardenScreen.tsx
+++ src/Screens/MyGardenScreen.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const MyGardenScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>MyGardenScreen</Text>
     </View>
   )
@@ -11,4 +11,10 @@ const MyGardenScreen = () => {
 
 export default MyGardenScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
